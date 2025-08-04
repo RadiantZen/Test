669b15581291d1dfded0d1954dbe0f4aa267bf21
RadiantZen <zen@radiantzen.net>
2025-08-04 19:58:50 +0200

    T

--- modules/default.nix
+++ modules/default.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, lib, ... }:
 
 {
   imports = [
@@ -6,31 +6,41 @@
     # ./cpu/intel
   ];
 
-  option.report = {
-    cpu = {
-      manufacturer = "amd";
-      model = "6700 XT";
-      architecture = "Zen 3";
-    };
-    gpu = {
-      design = "amd";
-      generation = "RDNA 2";
-      architecture = "Navi 22";
-      model = "6700 XT";
-      manufacturer = "xfx";
-      variation = "QICK 309";
-    };
-    motherboard = {
-      manufacturer = "ASRock";
-      chipset = "";
-      branding = "x470";
-      variation = "";
-      ram = {
-        slot0 = "";
-        slot1 = "";
-        slot2 = "";
-        slot4 = "";
-      };
+
+  options = {
+    report.cpu.manufacturer = lib.mkOption {
+      type = lib.types.string;
+      description = "Whether to enable AMD CPU Support.";
+      default = "amd";
+      example = true;
     };
   };
+
+  # option.report = {
+  #   cpu = {
+  #     manufacturer = "amd";
+  #     model = "6700 XT";
+  #     architecture = "Zen 3";
+  #   };
+  #   gpu = {
+  #     design = "amd";
+  #     generation = "RDNA 2";
+  #     architecture = "Navi 22";
+  #     model = "6700 XT";
+  #     manufacturer = "xfx";
+  #     variation = "QICK 309";
+  #   };
+  #   motherboard = {
+  #     manufacturer = "ASRock";
+  #     chipset = "";
+  #     branding = "x470";
+  #     variation = "";
+  #     ram = {
+  #       slot0 = "";
+  #       slot1 = "";
+  #       slot2 = "";
+  #       slot4 = "";
+  #     };
+  #   };
+  # };
 }
